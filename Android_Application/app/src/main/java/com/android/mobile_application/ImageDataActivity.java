524478c8a4fd17f0fa5b8ece31351fb1f3e51aa4
Sanket Chaudhari (Student) <schaud59@sundevils.asu.edu>
2022-12-17 00:40:39 -0700

    Code changes for MNIST Data classification

--- Android_Application/app/src/main/java/com/android/mobile_application/ImageDataActivity.java
+++ Android_Application/app/src/main/java/com/android/mobile_application/ImageDataActivity.java
@@ -1,34 +1,37 @@
 package com.android.mobile_application;
 
-
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 import java.io.ByteArrayOutputStream;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ImageDataActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -37,19 +40,9 @@ public class ImageDataActivity extends AppCompatActivity implements AdapterView.
     Spinner spinner;
     Button btnUploadImg;
     Bitmap bitmap;
-
-    TextView responseText;
-    Gateway gateway;
-    HashMap<String, BluetoothSocket> map;
-    List<String> accepteddevices;
-
+    EditText ipAddress;
+    EditText portNumber;
     private final String PATTERN = "^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}$";
-    private final String TAG = "MainActivity";
-
-    DigitClassifier digitClassifier0 = new DigitClassifier(this);
-    DigitClassifier digitClassifier1 = new DigitClassifier(this);
-    DigitClassifier digitClassifier2 = new DigitClassifier(this);
-    DigitClassifier digitClassifier3 = new DigitClassifier(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,10 +52,8 @@ public class ImageDataActivity extends AppCompatActivity implements AdapterView.
         //spinner = findViewById(R.id.directory);
         capturedImage = findViewById(R.id.image_view2);
         btnUploadImg = findViewById(R.id.btn_upload_img);
-
-        responseText = findViewById(R.id.responseText);
-        map=Master.map;
-        accepteddevices=Master.accepteddevices;
+        ipAddress = findViewById(R.id.ip_address);
+        portNumber = findViewById(R.id.port_number);
 
         Bundle bundle = getIntent().getExtras();
 
@@ -74,153 +65,92 @@ public class ImageDataActivity extends AppCompatActivity implements AdapterView.
         }
 
         // Listens for the upload button to get clicked
-        btnUploadImg.setOnClickListener((view) -> classifyDrawing(bitmap));
-
-        // Set up digit classifier
-        try {
-            digitClassifier0.initialize(0);
-            digitClassifier1.initialize(1);
-            digitClassifier2.initialize(2);
-            digitClassifier3.initialize(3);
-            Log.e(TAG, "Set up the classifier.");
-        }
-        catch (Exception e)
-        }
-
-
+        btnUploadImg.setOnClickListener((view) -> uploadImageToServer());
     }
 
-    public String BitMapToString(Bitmap bitmap){
-        ByteArrayOutputStream baos=new  ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG,100, baos);
-        byte [] b=baos.toByteArray();
-        String temp= Base64.encodeToString(b, Base64.DEFAULT);
-        return temp;
-    }
+    public void uploadImageToServer(){
+        String port = portNumber.getText().toString();
+        boolean isNumber = port.matches("[0-9]+");
+        String address = ipAddress.getText().toString();
 
-    /**
-     * @param encodedString
-     * @return bitmap (from given string)
-     */
-    public Bitmap StringToBitMap(String encodedString){
-        try {
-            byte[] decodedString = Base64.decode(encodedString, Base64.DEFAULT);
-            Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
-            return decodedByte;
-        } catch(Exception e) {
-            e.getMessage();
-            return null;
+        if(TextUtils.isEmpty(ipAddress.getText()) || !Pattern.matches(PATTERN, address)){
+            ipAddress.setError("Error in Ip Address");
+            ipAddress.requestFocus();
+        }
+        else if(TextUtils.isEmpty(portNumber.getText()) || !isNumber || Integer.parseInt(port) > 65536) {
+            portNumber.setError("Error in port number");
+            portNumber.requestFocus();
+        }else{
+            makeResponseBody();
         }
     }
 
-    private void classifyDrawing(Bitmap bitmap) {
+    public void makeResponseBody(){
+        ByteArrayOutputStream arrayStream = new ByteArrayOutputStream();
+        BitmapFactory.Options factoryOptions = new BitmapFactory.Options();
 
+        // Each pixel is stored on 2 bytes and only the RGB channels are encoded: red is stored with 5 bits of precision (32 possible values),
+        // green is stored with 6 bits of precision (64 possible values) and blue is stored with 5 bits of precision.
+        factoryOptions.inPreferredConfig = Bitmap.Config.RGB_565;
 
-        double[] arr = new double[10];
-        if ((bitmap != null)) {
-            DigitClassifier digitClassifier = null;
-            double max = 0.0;
-            int index = 0;
-            for(int i = 0; i < 4; i++){
-                switch(i){
-                    case 0:
-                        digitClassifier = digitClassifier0;
-                        break;
-                    case 1:
-                        digitClassifier = digitClassifier1;
-                        break;
-                    case 2:
-                        digitClassifier = digitClassifier2;
-                        break;
-                    case 3:
-                        digitClassifier = digitClassifier3;
-                        break;
-                }
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, arrayStream);
+        byte[] byteArray = arrayStream.toByteArray();
 
-                try {
-                    Bitmap bMap = preprocessImage(bitmap, i);
-                    String bitmap_str = BitMapToString(bMap);
-                    responseText.setText(digitClassifier.classify(bMap).toString());
-                    float[] res = digitClassifier.classify(bMap);
-                    double[] doubleArr = new double[10];
+        Long timeStampLong = System.currentTimeMillis()/1000;
+        String timeStamp = timeStampLong.toString();
 
-                    for(int j=0; j<10; j++){
-                        doubleArr[j] = (double)res[j];
-                        arr[j] = arr[j] + doubleArr[j] ;
+        RequestBody postBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("file", timeStamp + ".jpg", RequestBody.create(byteArray, MediaType.parse("image/*jpg")))
+                .build();
+
+        String postUrl= "http://" + ipAddress.getText().toString() + ":" + portNumber.getText().toString() +"/uploadImage";
+        // String postUrl= "http://192.168.0.101:5001/uploadImage";
+        postRequest(postUrl, postBody);
+    }
+
+    void postRequest(String postUrl, RequestBody postBody) {
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(postUrl)
+                .post(postBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                // To access the TextView inside the UI-thread, the code is added inside runOnUiThread()
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView responseText = findViewById(R.id.responseText);
+                        try {
+                            responseText.setText(response.body().string());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
+                });
+            }
 
-                    JSONObject jsonObject = new JSONObject();
-                    try {
-                        jsonObject.put("classify_image",i);
-                        jsonObject.put("data",1);
-                        jsonObject.put("quadrant", i);
-                        jsonObject.put("mat_img", Arrays.toString(doubleArr));
-                    } catch (JSONException e) {
-                        e.printStackTrace();
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                // Cancel the request on failure.
+                call.cancel();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView responseText = findViewById(R.id.responseText);
+                        responseText.setText("Failed to Connect to Server");
                     }
-                    String jsonString = jsonObject.toString();
-
-
-                    gateway = new Gateway(map.get(accepteddevices.get(i)), Master.handler);
-                    gateway.write(jsonString.getBytes());
-                } catch (Exception e) {
-                }
+                });
             }
-            for(int j=0; j<10; j++){
-                if(arr[j] > max){
-                    max = arr[j];
-                    index = j;
-                }
-            }
-            responseText.setText("Classification result: " + index);
-        }
+
+        });
     }
-
-    private Bitmap preprocessImage(Bitmap bitmap, int version) {
-        int width = bitmap.getWidth()/2;
-        int height = bitmap.getHeight()/2;
-        Bitmap bnw = bitmap.copy(bitmap.getConfig(),true);
-
-        int R, G, B;
-        int pixelValue;
-        for (int x = 0; x < bitmap.getWidth(); x++) {
-            for (int y = 0; y < bitmap.getHeight(); y++) {
-                // get pixel color
-                pixelValue = bitmap.getPixel(x, y);
-
-                R = Color.red(pixelValue);
-                G = Color.green(pixelValue);
-                B = Color.blue(pixelValue);
-                int gray = (int) (0.2989 * R + 0.5870 * G + 0.1140 * B);
-                if (gray < 128) {
-                    gray = 255;
-                }
-                else{
-                    gray = 0;
-                }
-                // set new pixel color to output bitmap
-                bnw.setPixel(x, y, Color.rgb(gray, gray, gray));
-            }
-        }
-        Bitmap croppedBitmap;
-        switch(version){
-            case 0:
-                croppedBitmap = Bitmap.createBitmap(bnw, 0, 0, width, height);
-                break;
-            case 1:
-                croppedBitmap = Bitmap.createBitmap(bnw, width, 0, width, height);
-                break;
-            case 2:
-                croppedBitmap = Bitmap.createBitmap(bnw, 0, height, width, height);
-                break;
-            default:
-                croppedBitmap = Bitmap.createBitmap(bnw, width, height, width, height);
-                break;
-        }
-        Log.e(TAG, "Leaving Cropping.");
-        return croppedBitmap;
-    }
-
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
@@ -231,15 +161,5 @@ public class ImageDataActivity extends AppCompatActivity implements AdapterView.
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
-    }
-
-    @Override
-    public void onDestroy() {
-        digitClassifier0.close();
-        digitClassifier1.close();
-        digitClassifier2.close();
-        digitClassifier3.close();
-
-        super.onDestroy();
     }
 }
